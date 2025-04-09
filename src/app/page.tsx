1fbf0ce9b43cb5467347ecf833554b9bb676aff2
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2025-04-09 19:24:40 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import Navbar, { NavBody, NavItems, NavbarLogo, NavbarButton } from '@/components/navbar'
+import React, { useState } from 'react'
+
+function Page() {
+  // Define your navigation items
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Features", link: "/features" },
+    { name: "Pricing", link: "/pricing" },
+    { name: "About", link: "/about" }
+  ];
+
   return (
-  <>
-     
-  </>
-  );
+    <div>
+      <Navbar>
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={navItems} className="flex-1" />
+          <div className="relative z-20 flex items-center justify-end gap-2">
+            <NavbarButton href="/login" variant="secondary">Login</NavbarButton>
+            <NavbarButton href="/signup">Sign Up</NavbarButton>
+          </div>
+        </NavBody>
+      </Navbar>
+    </div>
+  )
 }
+
+export default Page
